1ccd219b454bc832a9605bc34cfda609499f61df
Xelfter <150355101+Xelfter@users.noreply.github.com>
2025-01-18 19:52:20 +0100

    Itembase Better but not perfect

--- Source/Rider_Unreal_Test/Private/ItemBase.cpp
+++ Source/Rider_Unreal_Test/Private/ItemBase.cpp
@@ -2,4 +2,58 @@
 
 
 #include "ItemBase.h"
+#include "Rider_Unreal_Test/Data/ItemDataStructs.h"
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "Rider_Unreal_Test/Rider_Unreal_TestCharacter.h"
 
+
+
+UItemBase::UItemBase(): Quantity(0),
+	  ID(NAME_None),
+	  ItemType(EItemType::Consumable),
+	  ItemQuality(EItemQuality::Common),
+	  ItemStatistics({0.0f, 0.0f}),
+	  NumericData({0.0f, 1, false}),
+	  AssetData({nullptr, nullptr})
+{
+	
+}
+
+::UItemBase* ::UItemBase::CreateItemCopy() const
+{
+	UItemBase* ItemCopy = NewObject<UItemBase>(StaticClass());
+
+	ItemCopy->ID = this->ID;
+	ItemCopy->Quantity = this->Quantity;
+	ItemCopy->ItemQuality = this->ItemQuality;
+	ItemCopy->ItemType = this->ItemType;
+	ItemCopy->TextData = this->TextData;
+	ItemCopy->NumericData = this->NumericData;
+	ItemCopy->ItemStatistics = this->ItemStatistics;
+	ItemCopy->AssetData = this->AssetData;
+
+	return ItemCopy;
+}
+
+void ::UItemBase::SetQuantity(const int32 NewQuantity)
+{
+	if (NewQuantity != Quantity)
+	{
+		Quantity = FMath::Clamp(NewQuantity, 0,  NumericData.bIsStackable ? NumericData.MaxStackSize : 1);
+          
+         
+		//if (OwningInventory) 
+		{
+		//	if (Quantity <= 0) 
+			{
+			//	owningInventory->RemoveItem(this);
+			}
+		}
+	}
+};
+
+void ::UItemBase::Use(ARider_Unreal_TestCharacter* Character)
+{
+	
+}
--- Source/Rider_Unreal_Test/Public/ItemBase.h
+++ Source/Rider_Unreal_Test/Public/ItemBase.h
@@ -1,17 +1,76 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
+#include "Rider_Unreal_Test/Data/ItemDataStructs.h"
+#include "Rider_Unreal_Test/Rider_Unreal_TestCharacter.h"
 #include "ItemBase.generated.h"
 
-/**
- * 
- */
-UCLASS()
+
+UCLASS()	
 class RIDER_UNREAL_TEST_API UItemBase : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	//UPROPERTY()
+	//UInventoryComponent* OwningInventory;
 	
+	UPROPERTY(EditAnywhere, Category = "Item Data", meta = (UIMin=1, UIMax=100))
+	int32 Quantity;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FName ID;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemQuality ItemQuality;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemTextData TextData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemNumericData NumericData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemAssetData AssetData;
+	
+
+	UItemBase();
+
+	UFUNCTION(Category = "Item")
+	UItemBase* CreateItemCopy() const;
+
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float GetItemStackWeight() const { return Quantity * NumericData.Weight; };
+
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float GetItemSingleWeight() const { return NumericData.Weight; };
+	
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float IsFullStack() const { return Quantity * NumericData.MaxStackSize; };
+	
+	UFUNCTION(Category = "Item")
+	void SetQuantity(const int32 NewQuantity);
+	
+	UFUNCTION(Category = "Item")
+	virtual void Use(ARider_Unreal_TestCharacter* Character);
+
+	
+protected:
+	bool operator ==(const FName& OtherID) const 
+	{
+		return ID == OtherID;
+	}
 };
+
+
+//UCLASS()
+//class RIDER_UNREAL_TEST_API UItemBase : public UObject
+//{
+//	GENERATED_BODY()
+//};
