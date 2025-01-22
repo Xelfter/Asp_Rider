48b50063b7ae1fa4ce0693c3ac876a1af7a6ea97
Xelfter <150355101+Xelfter@users.noreply.github.com>
2025-01-22 01:28:50 +0100

    fixing

--- Source/Rider_Unreal_Test/UserInterface/InteractionWidget.cpp
+++ Source/Rider_Unreal_Test/UserInterface/InteractionWidget.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "UserInterface/InteractionWidget.h"
+#include "Rider_Unreal_Test/UserInterface/InteractionWidget.h"
 
